327f007229ce02197d0dfd193bdcb2f2d6c332e9
Aibek <s.aiba9320@gmail.com>
2023-03-15 01:23:22 +0600

    signup

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+const check = () => {
+    if(window.location.pathname === '/'){
+        if(localStorage.getItem('user') === null ){
+            window.location.replace('/pages/signup.html')
+        }
+    }
+}
+console.log(window.location.pathname === '/');
+check()
+
 const userData = (arr) => {
     const signupForm = document.getElementById('signup')
-    signupForm.addEventListener('submit', 
-    (event) =>{
+    signupForm && signupForm.addEventListener('submit', (event) =>{
         event.preventDefault()
-        console.log(signupForm);
+        const user = {}
+        Array.from(signupForm.children)
+        .filter(el => el.localName ==='input')
+        .forEach(el => {
+            user[el.type] = el.value
+        })
+        localStorage.setItem('user', JSON.stringify(user))
+        window.location.replace('/pages/login.html')
     })
 }
 userData()
