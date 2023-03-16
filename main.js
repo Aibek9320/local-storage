be7151daa7208ff33a7c1ee24039f081c0e9265d
Aibek9320 <123246995+Aibek9320@users.noreply.github.com>
2023-03-16 17:53:39 +0600

    aa

--- main.js
+++ main.js
@@ -8,18 +8,41 @@ const check = () => {
 console.log(window.location.pathname === '/');
 check()
 
+const USER = {}
+
 const userData = (arr) => {
     const signupForm = document.getElementById('signup')
     signupForm && signupForm.addEventListener('submit', (event) =>{
         event.preventDefault()
-        const user = {}
+        
         Array.from(signupForm.children)
         .filter(el => el.localName ==='input')
         .forEach(el => {
-            user[el.type] = el.value
+            USER[el.type] = el.value
         })
-        localStorage.setItem('user', JSON.stringify(user))
+        localStorage.setItem('user', JSON.stringify(USER))
         window.location.replace('/pages/login.html')
+        console.log(USER);
     })
 }
 userData()
+
+const logfunc = () => {
+    const loginForm = document.getElementById('loginfo')
+    loginForm && loginForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const userlog = {}
+        Array.from(loginForm.children)
+        .filter(el => el.localName === 'input')
+        .forEach(el => {
+            userlog[el.type] = el.value;
+        })
+        // const locstorageinfo = localStorage.getItem('user')
+        USER.filter(el => el.email )
+        
+        console.log(userlog);
+        console.log(USER)
+        signupData()
+    })
+}
+logfunc()
